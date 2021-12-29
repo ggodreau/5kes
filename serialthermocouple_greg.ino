dc43bfb150046c101b8c9227fa626bf25b4675b0
ggodreau <godreau@gmail.com>
2021-12-29 16:04:35 -0600

    adds blinky LED for offgrid

--- serialthermocouple_greg.ino
+++ serialthermocouple_greg.ino
@@ -1,13 +1,6 @@
 #include <SPI.h>
-//#include <Ticker.h>
 #include <TickTwo.h>
 #include "Adafruit_MAX31855.h"
-
-//Ticker everyOn;
-//Ticker everyOff;
-
-void getTemp();
-TickTwo cronTemp(getTemp, 2000, 0, MILLIS); // read every 0.5s, indefinitely
 
 // Pins
 const int MAXDO = 12; // D6 connects to 'DO' data out of MAX31855
@@ -15,7 +8,21 @@ const int MAXCS = 15; // D8 connects to 'CS' of MAX31855
 const int MAXCLK = 14; // D5 connects to 'CLK' of MAX31855
 Adafruit_MAX31855 thermocouple(MAXCLK, MAXCS, MAXDO);
 
+// Timers
+void getTemp();
+void connectAt();
+void ledBlink();
+TickTwo cronTemp(getTemp, 2000, 0, MILLIS); // temp read every 2s, indefinitely
+TickTwo cronTempBoot(getTemp, 250, 40, MILLIS); // read temp every 1/4s, 40 times (10s for bootup)
+TickTwo cronAtxBoot(connectAt, 250, 40, MILLIS); // bootstrap atx read every 1/4s, 40 times (10s for bootup)
+TickTwo cronLed(ledBlink, 50, 0, MILLIS); // blink led every 1/2s, indefinitely
+
 // Globals
+const int ledEsp = 2; // by the antenna
+const int ledMcu = 16; // by the mini usb connector
+bool ledEspState;
+bool ledMcuState;
+
 volatile bool tempAlarmState = false; // true = in alarm state
 double currentTemp; // MAX31855 will read 0.0F if it is disconnected
 double tempThresh = 80.0; // temp above which alarm is triggered
@@ -28,19 +35,19 @@ volatile long lastAtsDebounceTime = 0; // last millis() time ats debounce was tr
 
 bool atxState = false; // autotransformer state, true = on, false = off
 
-// Timer: Auxiliary variables
-//unsigned long now = millis();
-//unsigned long lastTrigger = 0;
-//boolean startTimer = false;
-
 void ledOn(){                        // this is your callback function
 //  Serial.println("led on");
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(ledEsp, LOW);
 }
 
 void ledOff(){                        // this is your callback function
 //  Serial.println("led off");
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(ledEsp, HIGH);
+}
+
+void ledBlink() {
+  digitalWrite(ledEsp, ledEspState);
+  ledEspState = !ledEspState;
 }
 
 void setup() {
@@ -49,10 +56,15 @@ void setup() {
   Serial.println("MAX31855 1 test");
   delay(2000);
   Serial.print("Initializing 1 sensor...");
-  pinMode(LED_BUILTIN, OUTPUT);
-  cronTemp.start();
+  pinMode(ledEsp, OUTPUT);
   pinMode(atsPin, INPUT_PULLUP);
+
+  cronTemp.start();
+  cronTempBoot.start();
+  cronAtxBoot.start();
+  
   atsState = digitalRead(atsPin);
+
   attachInterrupt(digitalPinToInterrupt(atsPin), atsAction, CHANGE);
   if (!thermocouple.begin()) {
     Serial.println("ERROR.");
@@ -63,32 +75,37 @@ void setup() {
 
 void loop() {
   cronTemp.update();
+  cronAtxBoot.update();
+  cronLed.update();
 }
 
 ICACHE_RAM_ATTR void atsAction(){
   int atsReading = digitalRead(atsPin);
-  Serial.println("atsReading: " + String(atsReading) + " atsState: " + String(atsState));
-
+//  Serial.println("atsReading: " + String(atsReading) + " atsState: " + String(atsState));
   if (atsReading == atsState) {
-//    Serial.println("atsReading == atsState");
     return;
   }
-  
-//  Serial.println("atsReading != atsState");
   boolean debounce = false;
-
   if ((millis() - lastAtsDebounceTime) <= atsDebounceDelay) {
     debounce = true;
   }
-  
   lastAtsDebounceTime = millis();
-  
   if (debounce) {
     Serial.println("less than debounce time, keeping state at: " + String(atsState));
     return;
   } else {
     Serial.println("setting state to: " + String(atsReading));
     atsState = atsReading;
+    if (atsState) {
+      Serial.println("ON grid");
+      cronLed.stop();
+      ledOn();
+    } else {
+      Serial.println("OFF grid");
+//      ledOff();
+      ledOff();
+      cronLed.start();
+    }
   }
 }
 
@@ -126,15 +143,25 @@ void handleTempDisAlarm() {
 }
 
 void disconnectAt() {
-  // TODO: flip AT relay pin OFF
-  Serial.println("Disconnecting AT relay");
-  atxState = false;
+  if (atxState) {
+    // TODO: flip AT relay pin OFF
+    Serial.println("Disconnecting AT relay");
+    atxState = false;
+  } else {
+    Serial.println("AT already disconnected");
+  }
 }
 
 void connectAt() {
   if (!currentTemp) {
     Serial.println("Cannot turn on AT without a valid temp reading");
-  } else {
+    return;
+  }
+  if (atsState) {
+    Serial.println("Cannot turn on AT while GW is on-grid");
+    return;
+  }
+  if (!atxState) {
     // TODO: flip AT relay pin ON
     Serial.println("Connecting AT relay");
     atxState = true;
